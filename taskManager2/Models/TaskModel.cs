4e40ea2adbc0c458e34c117e85bec95cc820bbcb
Michal <mbobas@gmail.com>
2021-03-10 22:13:45 +0100

    app is working without database

---
+++ taskManager2/Models/TaskModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace taskManager2.Models
+{
+    public class TaskModel
+    {
+        public int TaskId { get; set; }
+        [DisplayName("Nazwa")]
+        public string Name{ get; set; }
+        [DisplayName("Opis")]
+        public string  Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
